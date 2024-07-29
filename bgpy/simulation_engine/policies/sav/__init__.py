5bb4bde11502a54cbd325badf4256fbf5b98c62d
nscags <njscaglione@gmail.com>
2024-07-29 11:14:58 -0400

    troubleshooting

--- bgpy/simulation_engine/policies/sav/__init__.py
+++ bgpy/simulation_engine/policies/sav/__init__.py
@@ -1,10 +1,7 @@
 from .base_sav_policy import BaseSAVPolicy
 from .strict_urpf import StrictuRPF
 
-from .bgp_w_strict_urpf import BGPWithStrictuRPF
-
 __all__ = [
     "BaseSAVPolicy",
     "StrictuRPF",
-    "BGPWithStrictuRPF"
 ]
